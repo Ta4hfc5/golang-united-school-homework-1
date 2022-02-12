ffa7f4749918ba406411c1f8fffe2164fef59a1b
Ta4hfc5 <ta4hfc5@yandex.ru>
2022-02-12 21:51:49 +0300

    Commit HW-00 Upload-01

--- solution.go
+++ solution.go
@@ -1,5 +1,19 @@
-package solution
+package main
 
-func GetMessage() string {
-	return ""
+// + Инициализировать проект, создаёт файл go.mod: go mod init homework-01
+// + git checkout branchname
+// Выкачать зависимости для проекта: go mod tidy
+//
+// "Hello 🗺️!"
+// // Далее Commit
+// Push to server
+
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji"
+)
+
+func main() { // Имя функции с Большой буквы, чтобы была видна в других пакетах
+	fmt.Println(emoji.Sprint("Hello :world_map:!"))
 }
